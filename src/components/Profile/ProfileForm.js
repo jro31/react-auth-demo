ae4c6b0ed7311b5100babc5dbcf263bf7d5edd1c
Jethro Williams <jethrowilliams@outlook.com>
2021-11-03 14:43:32 +0000

    Automatically redirect the user to the homepage when they successfully change their password
    Almost identical code to the previous commit

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -29,7 +31,7 @@ const ProfileForm = () => {
       }
     ).then(res => {
       // Should add error handling. Here will assume that it always succeeds.
-      // Should provide some user feedback.
+      history.replace('/');
     });
   };
 

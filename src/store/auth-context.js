588ba8f5858df256b274ec811c7739995a4c824a
Jethro Williams <jethrowilliams@outlook.com>
2021-11-03 15:34:33 +0000

    Persist the user's authentication status in LocalStorage
    Once they've logged-in, they stay logged-in (even if the page reloads). Note that we haven't added any kind of expiration to this locally stored data, which is bad in case the token automatically expires after some time.

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,16 +8,22 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = props => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem('token'); // Will either be the stored token, or will be 'undefined' if the token doesn't exist
+  // Note that 'localStorage' is a synchronous API (hence we don't need to use async/await or anything similar)
+  const [token, setToken] = useState(initialToken);
+  // As the 'initialToken' is only used in the initial state, we won't override any state changes with the token
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = token => {
     setToken(token);
+    localStorage.setItem('token', token); // Side-note: localStorage can only store primitive data. If you want to store an object, for example, you have to convert it to JSON first (which would make it a string)
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token'); // Will remove the 'token' key/value (set above)
+    // An alternative to this would be to call 'localStorage.clear()', which erases all local storage for this site
   };
 
   const contextValue = {
